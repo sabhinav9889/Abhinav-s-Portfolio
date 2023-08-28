790021a07af678e5d989d33eb6d9ab442a2046ec
sabhinav9889 <sabhinav649@gmail.com>
2023-08-28 09:29:13 +0530

    nc

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css';
-// import Home from './components/Home';
-// import Nav from './components/Nav';
+import Home from './components/Home';
+import Nav from './components/Nav';
 import React from 'react';
 import About from './components/About';
-// import Projects from './components/Projects';
-// import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 import { Routes, Route} from 'react-router-dom';
 // import './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Nav/> */}
+      <Nav/>
       <Routes>
-        {/* <Route path='/' element={<Home/>}/> */}
+        <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
-        {/* <Route path='/projects' element={<Projects/>}/> */}
-        {/* <Route path='/skills' element={<Skills/>}/> */}
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/skills' element={<Skills/>}/>
       </Routes>
-      {/* <Nav/>
+      <Nav/>
       <Home/>
       <About/>
       <Projects/>
-      <Skills/> */}
+      <Skills/>
       {/* <Footer/> */}
     </div>
   );
